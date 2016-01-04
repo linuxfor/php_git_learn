e2888e3d7ff2398e9f628cdcd9cc552560a548c7
hyper <linuxfor@163.com>
2016-01-04 23:25:40 +0800

    fix the multiple array

--- php_array_multi2.php
+++ php_array_multi2.php
@@ -5,10 +5,10 @@
 <?php
 $cars = array
 (
-	array("Volvo", 33, 20);
-	array("BMW", 17, 15);
-	array("Saab", 5, 2);
-	array("Land Rover", 15, 11);
+	array("Volvo", 33, 20),
+	array("BMW", 17, 15),
+	array("Saab", 5, 2),
+	array("Land Rover", 15, 11)
 );
 
 for ($row = 0; $row < 4; $row++) {
