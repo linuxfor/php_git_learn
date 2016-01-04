6469c88f817d9e9babff00e309dc03035530ebb3
hyper <linuxfor@163.com>
2016-01-05 01:26:38 +0800

    using strtotime for unix timestamp

--- php_date2_unix_timestamp.php
+++ php_date2_unix_timestamp.php
@@ -4,8 +4,13 @@
 
 <?php
 // 01/05/2016 01:18:32am
+//mktime(hour, minute, second, month, day, year)
 $d = mktime(1, 18, 32, 1, 5, 2016);
-echo "Created date is " . date("m/d/Y h:i:sa", $d);
+echo "Created date is " . date("m/d/Y h:i:sa", $d) . "<br>";
+
+//strtotime(time, now)
+$d = strtotime("12:19pm January 05 2016");
+echo "Created date is " . date("Y-m-d h:i:sa", $d);
 ?>
 
 </body>
