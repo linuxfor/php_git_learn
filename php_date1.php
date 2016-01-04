ae111f3c307495d0afe132439632f6fa744ea459
hyper <linuxfor@163.com>
2016-01-04 23:43:02 +0800

    get simple time

--- php_date1.php
+++ php_date1.php
@@ -3,10 +3,13 @@
 <body>
 
 <?php
+//date()函数会返回服务器的当前日期和时间
+//不一定和实际的时间同步
 echo "Today is " . date("Y/m/d") . "<br>";
 echo "Today is " . date("Y.m.d") . "<br>";
 echo "Today is " . date("Y-m-d") . "<br>";
-echo "The year is " . date("Y");
+echo "The year is " . date("Y") . "<br>";
+echo "The current time is " . date("h:i:sa") . "<br>";
 ?>
 
 </body>
