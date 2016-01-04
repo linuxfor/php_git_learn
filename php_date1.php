dcafa7f4c7b33bc553ee860742e4ecb4b300541b
hyper <linuxfor@163.com>
2016-01-04 23:26:13 +0800

    date function sample

---
+++ php_date1.php
@@ -0,0 +1,13 @@
+<!DOCTYPE>
+<html>
+<body>
+
+<?php
+echo "Today is " . date("Y/m/d") . "<br>";
+echo "Today is " . date("Y.m.d") . "<br>";
+echo "Today is " . date("Y-m-d") . "<br>";
+//echo "Today is " . date("1");
+?>
+
+</body>
+</html>
