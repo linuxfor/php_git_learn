441ac9d14c6c240991d1197667f584d7ea309de0
hyper <linuxfor@163.com>
2016-01-05 01:32:58 +0800

    function strtotime testing

--- php_date2_unix_timestamp.php
+++ php_date2_unix_timestamp.php
@@ -10,7 +10,17 @@ echo "Created date is " . date("m/d/Y h:i:sa", $d) . "<br>";
 
 //strtotime(time, now)
 $d = strtotime("12:19pm January 05 2016");
-echo "Created date is " . date("Y-m-d h:i:sa", $d);
+echo "Created date is " . date("Y-m-d h:i:sa", $d) . "<br>";
+?>
+
+<?php
+$startdate = strtotime("Saturday");
+$enddate = strtotime("+6 weeks", $startdate);
+
+while ($startdate < $enddate) {
+	echo date("M d", $startdate), "<br>";
+	$startdate = strtotime("+1 week", $startdate);
+}
 ?>
 
 </body>
