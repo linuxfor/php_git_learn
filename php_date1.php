4de2cc700359ca4c980e2081402d5b3e8851b203
hyper <linuxfor@163.com>
2016-01-04 23:50:18 +0800

    fix the time zone settings

--- php_date1.php
+++ php_date1.php
@@ -5,7 +5,7 @@
 <?php
 
 //setup the time zone
-date_default_timezone_set(Asia/Shanghai);
+date_default_timezone_set("Asia/Shanghai");
 
 //date()函数会返回服务器的当前日期和时间
 //不一定和实际的时间同步
@@ -14,6 +14,15 @@ echo "Today is " . date("Y.m.d") . "<br>";
 echo "Today is " . date("Y-m-d") . "<br>";
 echo "The year is " . date("Y") . "<br>";
 echo "The current time is " . date("h:i:sa") . "<br>";
+
+/*
+//tested result:
+Today is 2016/01/04
+Today is 2016.01.04
+Today is 2016-01-04
+The year is 2016
+The current time is 11:46:36pm
+*/
 ?>
 
 </body>
