5d59f7cdac91f19d65e323657af527e4a8597ec3
hyper <linuxfor@163.com>
2016-01-06 09:29:55 +0800

    php include file & footer.php

---
+++ php_include.php
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML>
+<html>
+<body>
+
+<h1>Welcome to access my homepage</h1>
+<p>This is a paragraph</p>
+<p>This is another paragraoh</p>
+<?php include 'footer.php';?>
+
+</body>
+</html>
