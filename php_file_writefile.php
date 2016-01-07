3cbc3ca576bbea1b2fe27c6a99a5f8eb65ef243a
hyper <linuxfor@163.com>
2016-01-08 00:41:47 +0800

    php file to write

---
+++ php_file_writefile.php
@@ -0,0 +1,8 @@
+<?php
+$myfile = fopen("newfile_for_write.txt", "w") or die("Unable to open file!");
+$txt = "Bill Gates\n";
+fwrite($myfile, $txt);
+$txt = "Steve Jobs\n";
+fwrite($myfile, $txt);
+fclose($myfile);
+?>
