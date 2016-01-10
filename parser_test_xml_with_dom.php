2c5d8920de419cec58d4648797057faa306fa1b5
hyper <linuxfor@163.com>
2016-01-11 03:18:51 +0800

    print the element in circle

--- parser_test_xml_with_dom.php
+++ parser_test_xml_with_dom.php
@@ -2,5 +2,23 @@
 $xmlDoc = new DOMDocument();
 $xmlDoc->load("test.xml");
 
-print $xmlDoc->saveXML();
+//print $xmlDoc->saveXML();
+$x = $xmlDoc->documentElement;
+foreach ($x->childNodes as $item)
+{
+	print $item->nodeName." = ".$item->nodeValue."<br/>";
+}
+
+/*
+//test result
+#text = 
+to = George
+#text = 
+from = John
+#text = 
+heading = Reminder
+#text = 
+body = Don't forget the meeting!
+#text = 
+*/
 ?>
