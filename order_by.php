403ca036f87e4271cab9cbd681fe753b6f869644
hyper <linuxfor@163.com>
2016-01-11 00:56:34 +0800

    mysql order by

---
+++ order_by.php
@@ -0,0 +1,21 @@
+<?php
+$con = mysql_connect("localhost", "hyper", "hyper@5");
+if (!$con)
+{
+	die('Could not connect: '.mysql_error());
+}
+
+mysql_select_db("my_db", $con);
+
+$result = mysql_query("SELECT * FROM Persons ORDER BY age");
+
+while($row = mysql_fetch_array($result))
+{
+	echo $row['FirstName'];
+	echo " ".$row['LastName'];
+	echo " ".$row['Age'];
+	echo "<br />";
+}
+
+mysql_close($con);
+?>
